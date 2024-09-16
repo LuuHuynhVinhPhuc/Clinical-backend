f62ae54ad8e5d3af08603019c6cddc001eeba371
haifam <haiphamtrung62@gmail.com>
2024-09-16 16:07:18 +0700

    feat: add IAuditable interceptop

--- src/Infrastructure/Persistence/Interceptors/AuditableEntitiesInterceptor.cs
+++ src/Infrastructure/Persistence/Interceptors/AuditableEntitiesInterceptor.cs
@@ -2,7 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Diagnostics;
-using System;
 
 namespace ClinicalBackend.Persistence.Interceptors
 {

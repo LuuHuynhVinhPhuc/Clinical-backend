68f0a3a0b0015c79bb805779c2dfd43a22eb37d8
MarcusLuu <vinhphucluuhuynh@gmail.com>
2024-09-27 14:23:16 +0700

    feat: Prescription feature

--- src/Core/Domain/Entities/Prescription.cs
+++ src/Core/Domain/Entities/Prescription.cs
@@ -7,12 +7,37 @@ namespace ClinicalBackend.Domain.Entities
     {
         public Guid PatientID { get; set; }
         
-
+        // IAuditable
         public DateTime CreatedAt { get; set; }
         public DateTime ModifiedAt { get; set; }
 
         //Navigation property
         public virtual Medicine Medicine { get; set; }
         public virtual Patient Patient { get; set; }
+
+        // Props
+        // Patient
+        public string? PatientName { get; set; }
+        public int Age { get; set; }
+        public string? Address { get; set; }
+        public string? PhoneNumber { get; set; }
+
+        // Follow UP
+        public string? Summary { get; set; }
+
+        // Prescription
+        public List<PrescriptionDrug> PrescriptionDrugs { get; set; } = new List<PrescriptionDrug>();
+    }
+
+    public class PrescriptionDrug
+    {
+        public Guid MedicineID { get; set; }
+        public virtual Medicine Medicine { get; set; }
+        public string Name { get; set; }
+        public int Morning { get; set; }
+        public int Noon { get; set; }
+        public int Night { get; set; }
+        public string? Usage { get; set; }
+        public string? Note { get; set; }
     }
 }

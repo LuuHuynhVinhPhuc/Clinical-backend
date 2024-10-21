1a19bb48860863d607662b0bbc6bab02002c2b72
maxipon <phi1077000@gmail.com>
2024-10-21 22:35:21 +0700

    feat: Prescription Repository

--- src/Infrastructure/Persistence/Repositories/PrescriptionRepository.cs
+++ src/Infrastructure/Persistence/Repositories/PrescriptionRepository.cs
@@ -2,6 +2,7 @@
 using ClinicalBackend.Domain.Repositories;
 using ClinicalBackend.Persistence.Context;
 using Domain.Common;
+using Microsoft.EntityFrameworkCore;
 
 namespace ClinicalBackend.Persistence.Repositories
 {
@@ -9,6 +10,59 @@ namespace ClinicalBackend.Persistence.Repositories
     {
         public PrescriptionRepository(ApplicationDbContext context) : base(context)
         {
+        }
+
+        public override async Task<IEnumerable<Prescription>> GetAllAsync(int pageNumber, int pageSize, CancellationToken cancellationToken = default)
+        {
+            return await dbSet
+                .Include(p => p.Patient)
+                .Include(p => p.FollowUp)
+                .Include(p => p.Products)
+                .ThenInclude(p => p.Medicine)
+                .OrderByDescending(p => p.CreatedAt)
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync()
+                .ConfigureAwait(false);
+        }
+
+        public async Task<Prescription> GetByIdAsync(Guid Id)
+        {
+            return await dbSet
+                .Include(p => p.Patient)
+                .Include(p => p.FollowUp)
+                .FirstOrDefaultAsync(p => p.Id == Id)
+                .ConfigureAwait(false);
+        }
+
+        public async Task<IEnumerable<Prescription>> GetByPatientIdAsync(Guid patientId)
+        {
+            return await dbSet
+                .Include(p => p.Patient)
+                .Include(p => p.FollowUp)
+                .Where(p => p.PatientId == patientId)
+                .ToListAsync()
+                .ConfigureAwait(false);
+        }
+
+        public async Task<int> GetTotalCountAsync() // Add this method
+        {
+            return await dbSet.CountAsync().ConfigureAwait(false);
+        }
+
+        public async Task<int> GetTotalCountByNameAsync(string Name)
+        {
+            return await dbSet.CountAsync(p => p.Patient.Name == Name).ConfigureAwait(false);
+        }
+
+        public async Task<IEnumerable<Prescription>> SearchByNameAsync(string Name, int pageNumber, int pageSize)
+        {
+            return await dbSet
+                .Include(p => p.Patient)
+                .Include(p => p.FollowUp)
+                .Where(p => p.Patient.Name == Name)
+                .ToListAsync()
+                .ConfigureAwait(false);
         }
     }
 }

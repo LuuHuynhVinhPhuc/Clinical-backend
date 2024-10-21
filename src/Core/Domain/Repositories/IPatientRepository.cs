80a2ad638ff46d1aec547eb0aae112bf1aecf469
maxipon <phi1077000@gmail.com>
2024-10-21 22:53:21 +0700

    chore: added GetCount to Repository Interface for Patient

--- src/Core/Domain/Repositories/IPatientRepository.cs
+++ src/Core/Domain/Repositories/IPatientRepository.cs
@@ -7,13 +7,10 @@ namespace ClinicalBackend.Domain.Repositories
     {
         Task<Patient> GetByIdAsync(Guid id);
         Task<IEnumerable<Patient>> GetAllAsync(int pageNumber, int pageSize);
-
-        // search with Name 
         Task<IEnumerable<Patient>> FindWithNameAsync(string name, int pageNumber, int pageSize);
-
-        // search with phone number 
         Task<Patient> FindWithPhoneNumberAsync(string phoneNumber);
         Task<int> GetTotalCountAsync();
         Task<IEnumerable<Patient>> GetPatientByDateAsync(DateTime dateStart, DateTime dateEnd, int pageNumber, int pageSize);
+        Task<int> GetTotalCountByDateAsync(DateTime dateStart, DateTime dateEnd);
     }
 }

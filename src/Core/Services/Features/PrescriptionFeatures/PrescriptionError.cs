9a12600e63c8ecc6cc7ec9069a23f99d4f4fd01a
maxipon <phi1077000@gmail.com>
2024-10-21 22:49:35 +0700

    feat: Prescription errors

--- src/Core/Services/Features/PrescriptionFeatures/PrescriptionError.cs
+++ src/Core/Services/Features/PrescriptionFeatures/PrescriptionError.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ClinicalBackend.Services.Common;
 
 namespace ClinicalBackend.Services.Features.PrescriptionFeatures
 {
-    public class PrescriptionError
+    public static class PrescriptionError
     {
+        public static readonly Error PrescriptionExist = new Error("Prescriptions.PrescriptionExist", "Prescription already exist");
+        
+        public static Error NotFound(string Name) => new Error(
+        "Prescription.NotFound", $"Prescription with the name '{Name}' was not found");
+
+        // Prescription ID is not exist
+        public static Error IDNotFound(Guid ID) => new Error(
+        "Prescription.NotFound", $"Prescription with the ID '{ID}' was not found");
+
+        // No prescription found for the given date
+        public static Error NoPrescriptionFoundForDate(string date) => new Error(
+        "Prescription.NotFound", $"No prescription found for the date: {date}");
     }
 }

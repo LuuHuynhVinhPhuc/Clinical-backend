d409a1467ea1dea60e553c723427b6f11dea5b4a
maxipon <phi1077000@gmail.com>
2024-10-21 22:51:18 +0700

    feat: Mapper for Prescription

---
+++ src/Core/Services/Configs/Mappers/Prescription/PrescriptionMapper.cs
@@ -0,0 +1,32 @@
+using ClinicalBackend.Contracts.DTOs.FollowUp;
+using ClinicalBackend.Contracts.DTOs.Medicine;
+using ClinicalBackend.Contracts.DTOs.Patient;
+using ClinicalBackend.Contracts.DTOs.Prescription;
+using ClinicalBackend.Domain.Entities;
+using Mapster;
+
+namespace ClinicalBackend.Services.Configs.Mappers
+{
+    public class PrescriptionMapper : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<Prescription, GetPrescriptionDto>()
+                .MapWith(src => new GetPrescriptionDto(
+                    src.Id,
+                    src.Patient.Adapt<PatientDto>(),
+                    src.FollowUp.Adapt<SummaryDto>(),
+                    src.Products.Adapt<ICollection<GetProductDto>>(),
+                    src.Notes,
+                    src.BillDate,
+                    src.CreatedAt
+                ));
+
+            config.NewConfig<Product, GetProductDto>()
+                .MapWith(src => new GetProductDto(
+                    src.Medicine.Name,
+                    src.Quantity,
+                    src.Medicine.Instructions.Adapt<InstructionsDto>()));
+        }
+    }
+}

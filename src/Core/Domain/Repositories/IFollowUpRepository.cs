822a0ed85c28d5bf0ef3b0d586407f7849983cac
maxipon <phi1077000@gmail.com>
2024-10-21 22:52:40 +0700

    feat: added GetByPatientAsync to Repository for FollowUp

--- src/Core/Domain/Repositories/IFollowUpRepository.cs
+++ src/Core/Domain/Repositories/IFollowUpRepository.cs
@@ -6,9 +6,8 @@ namespace ClinicalBackend.Domain.Repositories
     public interface IFollowUpRepository : IBaseRepository<FollowUp>
     {
         Task<IEnumerable<FollowUp>> GetAllAsync(int pageNumber, int pageSize, CancellationToken cancellationToken = default);
-
         Task<FollowUp> GetByIdAsync(Guid Id);
-
+        Task<IEnumerable<FollowUp>> GetByPatientIdAsync(Guid patientId);
         Task<int> GetTotalCountAsync();
     }
 }

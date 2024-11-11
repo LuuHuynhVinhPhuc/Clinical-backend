643ac60a1e0571b28f118b0de40f221d6ba7f022
maxipon <phi1077000@gmail.com>
2024-11-11 15:16:57 +0700

    feat: additional properties & removing Type

--- src/Core/Domain/Entities/Medicine.cs
+++ src/Core/Domain/Entities/Medicine.cs
@@ -7,15 +7,17 @@ namespace ClinicalBackend.Domain.Entities
     {
         public string? Name { get; set; }
         public string? Company { get; set; }
+        public string? Specialty { get; set; }
+        public IList<string> Nutritional { get; set; } = new List<string>(); // Changed to IList<string>
+        public string? Dosage { get; set; }
         public int Stock { get; set; }
         public float Price { get; set; }
         public string? Status { get; set; }
-        public string? Type { get; set; }
 
         public DateTime CreatedAt { get; set; }
         public DateTime ModifiedAt { get; set; }
 
-        // public string? Dosage { get; set; }
+        // public string? Dosage { get; set; } = dạng bào chế
         // public string? Instructions { get; set; }
         // public string? SideEffects { get; set; }
         // public string? Contraindications { get; set; }

c03a60375d3afa4fae16b4f4cd004693b4257b9f
maxipon <phi1077000@gmail.com>
2024-11-11 15:18:52 +0700

    feat: added some new methods

--- src/Core/Domain/Repositories/IMedicineRepository.cs
+++ src/Core/Domain/Repositories/IMedicineRepository.cs
@@ -8,8 +8,11 @@ namespace ClinicalBackend.Domain.Repositories
         Task<IEnumerable<Medicine>> SearchByNameAsync(string Name, int pageNumber, int pageSize);
         Task<IEnumerable<Medicine>> GetAllAsync(int pageNumber, int pageSize, CancellationToken cancellationToken = default); 
         Task<Medicine> GetByIdAsync(Guid Id);
+        Task<Medicine> GetByNameAsync(string name);
         Task<int> GetTotalCountAsync(); 
         Task<int> GetTotalCountByNameAsync(string Name);
+        Task<IEnumerable<Medicine>> SearchBySpecialtyAsync(string Specialty, int pageNumber, int pageSize);
+        Task<int> GetTotalCountBySpecialtyAsync(string Specialty);
         Task<int> GetTotalCountByDateAsync(DateTime dateStart, DateTime dateEnd);
         Task<IEnumerable<Medicine>> GetMedicinesByDateAsync(DateTime dateStart, DateTime dateEnd, int pageNumber, int pageSize);
     }
